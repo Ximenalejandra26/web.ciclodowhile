be352e26f59afefad0b031e7de60f6859aa31822
ximena <ximena.aleja021@outlook.com>
2024-11-04 21:40:07 -0500

    Si el número ingresado no es 0000, lo sumamos al total con total += numero

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 let total = 0;
 let numero; 
 // Usamos un do...while para seguir pidiendo números hasta que se ingrese "0000"
+do {
+    numero = parseInt(prompt("Introduce un número para sumar. Ingresa 0000 para finalizar:"));
+
+    if (numero !== 0000) {
+        total += numero; 
+    }
+
+}
